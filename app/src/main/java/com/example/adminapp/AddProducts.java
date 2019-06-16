7ffd0a2b28ac8467db8294be5d883bdba54c63f1
gvgh <sanchirastogi02@gmail.com>
2019-06-16 17:03:17 +0530

    add product activity

--- app/src/main/java/com/example/adminapp/AddProducts.java
+++ app/src/main/java/com/example/adminapp/AddProducts.java
@@ -3,12 +3,29 @@ package com.example.adminapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddProducts extends AppCompatActivity {
+
+    private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputproductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_products);
+
+        CategoryName = getIntent().getExtras().get("Category").toString();
+
+        AddNewProductButton = findViewById(R.id.add_product_button);
+        InputProductImage = findViewById(R.id.add_product_image);
+        InputProductName = findViewById(R.id.add_product_name);
+        InputProductDescription = findViewById(R.id.add_product_description);
+        InputproductPrice = findViewById(R.id.add_product_price);
+
     }
 }
