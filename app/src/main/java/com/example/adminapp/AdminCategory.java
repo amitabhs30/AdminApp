b6d4dcbb18cd899829be6ba9ba0bf793b2b8ecf6
gvgh <sanchirastogi02@gmail.com>
2019-06-16 19:59:33 +0530

    category and add product activity added

---
+++ app/src/main/java/com/example/adminapp/AdminCategory.java
@@ -0,0 +1,42 @@
+package com.example.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class AdminCategory extends AppCompatActivity {
+
+    private LinearLayout linearLayout1, linearLayout2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        linearLayout1 = findViewById(R.id.add_product_selfie);
+
+        linearLayout2 = findViewById(R.id.add_product_card);
+
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategory.this, AddProducts.class);
+                intent.putExtra("Category", "AR Selfie ");
+                startActivity(intent);
+            }
+        });
+
+        linearLayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategory.this, AddProducts.class);
+                intent.putExtra("Category", "AR Card ");
+                startActivity(intent);
+            }
+        });
+    }
+
+}
